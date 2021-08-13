af1bb13093658435abf2977ecd0bfc77304661c7
unknown <toomkira@gmail.com>
2021-08-13 23:30:53 +0200

    setup app for heruko

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express')
 const app = express();
+const port = process.env.PORT || 3000
 const hbs = require('hbs');
 const request = require('request');
 const geocode = require('./utiles/geocode');
@@ -104,6 +105,6 @@ app.get('*', (req,res) =>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
